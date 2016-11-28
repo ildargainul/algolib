1c1de99d64146a2ff28853e7df6c027874dc3211
Ildar Gainullin <etodisnavseh@mail.ru>
2016-11-28 17:47:14 +0300

    Update minqueue.cpp

--- minqueue.cpp
+++ minqueue.cpp
@@ -1,17 +1,3 @@
-#include <iostream>
-#include <fstream>
-#include <algorithm>
-#include <vector>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <stack>
-
-using namespace std;
-
-typedef unsigned int index;
-
 const int INF = 1e9 + 1;
 
 class MinStack 
@@ -78,34 +64,3 @@ class MinQueue
             spop.pop();
         }
 };
-
-int main()
-{
-#ifdef ONPC
-    freopen("a.in", "r", stdin);
-    //freopen("a.out", "w", stdout);
-#else
-    //freopen("a.in", "r", stdin);
-    //freopen("a.out", "w", stdout);
-#endif
-    ios::sync_with_stdio(0);
-    size_t n, k;
-    int x;
-    MinQueue q;
-    cin >> n >> k;
-    for (index i = 0; i < k; i++)
-    {
-        cin >> x;
-        q.push(x);
-    }
-    cout << q.min() << ' ';
-    for (index i = (index) k; i < n; i++)
-    {
-        q.pop();
-        cin >> x;
-        q.push(x);
-        cout << q.min() << ' ';
-    }
-    cout << '\n';
-}
-
